fb156659cbbbd3990d243cfceda1fc685fe1ced5
Frederik Greve Petersen <frederik.greve@gmail.com>
2019-12-03 10:33:06 +0100

    Mic og kvadrant update

--- Hue lights/connect.js
+++ Hue lights/connect.js
@@ -11,18 +11,49 @@ let hueSlider;
 let briSlider;
 let ctSlider;
 
-const lightNumber = 16;
+//Kvadranter
+let kvadrant1, kvadrant2, kvadrant3, kvadrant4;
+let kvadrantPlads;
+
+//Mic
+let mic, volume;
+
+
+//Buffer
+let micBuf = [];
+let highestVol = 0;
+let VolCounter = 0;
+let VolCounterLimit = 60 * 30;
+let bufferMinutes = 5;
+let bufferSize = (60 * 60 * bufferMinutes) / VolCounterLimit;
+
+const lightNumber = 24;
 
 function setup(){
-    createCanvas(0,0);
+    createCanvas(500, 500);
     createElement("h1", lightNumber);
+
+// ---------- Kvadranter ---------- //
+    kvadrant1 = createButton("Kvadrant 1", 1).size(100, 100).position(0, 0).mousePressed(prom);
+    kvadrant2 = createButton("Kvadrant 2", 2).size(100, 100).position(100, 0).mousePressed(prom);
+    kvadrant3 = createButton("Kvadrant 3", 3).size(100, 100).position(0, 100).mousePressed(prom);
+    kvadrant4 = createButton("Kvadrant 4", 4).size(100, 100).position(100, 100).mousePressed(prom);
+
+// ---------- Slut ---------- //
+
+// ---------- Mic ---------- //
+    mic = new p5.AudioIn();
+    mic.start()
+
+// ---------- Slut ---------- //
+
 
     //conBut = createButton("connect").mousePressed(connect);
 
-    hueBut = createButton("hue").mousePressed(setHue);
-    hueSlider = createSlider(0, 65534, 0, 1);
-    createDiv("hue value: " + hueSlider.value());
-    createElement("br");
+    // hueBut = createButton("hue").mousePressed(setHue);
+    // hueSlider = createSlider(0, 65534, 0, 1);
+    // createDiv("hue value: " + hueSlider.value());
+    // createElement("br");
 
     briBut = createButton("brightness").mousePressed(setBri);
     briSlider = createSlider(1, 254, 1, 1);
@@ -41,6 +72,35 @@ function setup(){
 }
 
 function draw(){
+    // Sæt lydniveau
+    volume = mic.getLevel();
+
+    //Logger det højeste lydniveau
+    VolCounter++;
+    //console.log("Counter: " + VolCounter);
+    if(volume > highestVol){
+        highestVol = volume;
+        console.log("Højeste Volume: " + highestVol);
+    }
+    if(VolCounter >= VolCounterLimit){
+        // Laver en buffer på lydniveauet
+        if(micBuf.length < bufferSize){
+            micBuf.push(highestVol);
+        }else{
+            micBuf.splice(1, 1);
+            micBuf.push(highestVol);
+        }
+        console.log("Buffer længde: " + micBuf.length)
+        console.log(micBuf);
+        VolCounter = 0;
+        highestVol = 0;
+
+    }
+
+
+
+    
+    
     /*
     if(mouseIsPressed && frameCount%90){
         setHue(hueSlider.value);
@@ -104,4 +164,11 @@ function setct(){
     }
 
     setLight(lightNumber, ct);
+}
+
+function prom(){
+    if(confirm("Er det den rigtige kvadrant " + this.elt.value + " ?")){
+        kvadrantPlads = this.elt.value;
+        console.log(kvadrantPlads);
+    }
 }
